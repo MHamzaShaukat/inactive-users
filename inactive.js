cf80181dd3b64a0d88104cc618d05a741f018659
Muhammad Hamza Shaukat <52293925+MHamzaShaukat@users.noreply.github.com>
2021-02-23 03:31:09 +0500

    Update inactive.js
    updated the code (testing took time)

--- inactive.js
+++ inactive.js
@@ -21,4 +21,20 @@ const inactiveThirdStage = () => {
 // this is the main method that acts like a worker and runs every 1 minute
 setInterval(function(){
     // please write your code here
-}, 1000);
+    let date= new Date().getTime();
+     let diffSeconds = (date - user.lastActivity )/1000;
+     diffSeconds = diffSeconds % 300;
+     if( diffSeconds >= 60 && diffSeconds <=120){
+         inactiveFirstStage()
+     }
+
+     else if ( diffSeconds >= 120 && diffSeconds <=180){
+         inactiveSecondStage()
+     }
+
+     else if ( diffSeconds >= 240 && diffSeconds <=300){
+        inactiveThirdStage()
+    }
+     
+console.log("called",diffSeconds)
+}, 60000);
